9ba0c6f1cc3358549318bbde0a5972e27ddc8c56
jayakrishnan004 <jayakrishnanvenugopal.2004@gmail.com>
2025-11-11 14:44:58 +0530

    day1

---
+++ Day 1/first.py
@@ -0,0 +1,46 @@
+#this is a challenge month
+#learn python
+
+print("hello pycharm")
+print("i am going to start python")
+
+
+#variables
+first_name = "jk"
+print(first_name)
+
+#strings
+#f-string
+name = "jk"
+email="jk@gmail.com"
+print(f"hello{name}")
+print(f"mail is {email}")
+
+#integers
+age = 21
+quantity = 5
+num = 50
+print(f"my age is : {age}")
+print(f"you are buying {quantity} items")
+print(f"your class has {num} students")
+
+#float
+price = 10.99
+distance = 5.5
+print(f"the price is ${price}")
+print(f"you ran {distance} kms")
+
+#boolean
+is_student = True
+print(f'are you a student?:{is_student}')
+
+if is_student:
+    print("you are a student")
+else:
+    print("you are not a student")
+
+for_sale = True
+if for_sale:
+    print("the item is for sale")
+else:
+    print("the item is not for sale")
