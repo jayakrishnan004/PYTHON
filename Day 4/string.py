5df307bfd6bdc1e49cb2693fa76add0c157b92a8
jayakrishnan004 <jayakrishnanvenugopal.2004@gmail.com>
2025-11-23 13:45:49 +0530

    string indexing

---
+++ Day 4/string.py
@@ -0,0 +1,30 @@
+name = input("Enter your name:")
+
+# result = len(name)
+# result = name.find("k")
+
+#needs last occurence
+# result = name.rfind("a")
+
+
+#result = name.capitalize()
+#makes the first letter capital
+
+
+#makes uppercase and lowercase
+#result = name.upper()
+#result = name.lower()
+
+
+#result = name.isdigit()
+#result = name.isalpha()
+
+#result = name.count("a")
+
+result = name.replace("a","-")
+
+print(result)
+
+
+#for getting help
+#print(help(str))
