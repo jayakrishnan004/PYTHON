579126c2999bd60b9c6bc383909798a10966b3ec
jayakrishnan004 <jayakrishnanvenugopal.2004@gmail.com>
2025-11-23 13:41:54 +0530

    conditional expressions

---
+++ Day 3/conditional_expression.py
@@ -0,0 +1,16 @@
+#conditional operators
+
+x=5
+y=7
+#max = (x if x>y else y)
+#min = (x if x<y else y)
+
+# age = 5
+# result = ("limited access" if age<18 else "full access")
+
+# num = 67
+# result = ("even number" if num%2 == 0 else "number is odd")
+
+temp = 14
+result = "hot" if temp>25 else "cold"
+print(result)
